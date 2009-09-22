f3cdde0e5963adabe30573cfc2dcd75e85a0c64b
Dan Hackney <dan@haxney.org>
2009-09-22 02:14:38 -0400

    Remove redundant "int-to-string"
    The `format' function already takes care of this.
    
    Signed-off-by: Dan Hackney <dan@haxney.org>

--- ezbl.el
+++ ezbl.el
@@ -649,7 +649,7 @@ This 'ezbl instance' is used in various other functions."
       (with-current-buffer output-buffer
         (rename-buffer (format ezbl-output-buffer-format (int-to-string pid)))
         (set (make-local-variable 'ezbl-instance) instance))
-      (rename-buffer (format ezbl-display-buffer-format (int-to-string pid)))
+      (rename-buffer (format ezbl-display-buffer-format pid))
       (set (make-local-variable 'ezbl-instance) instance)
 
       (add-to-list 'ezbl-instances `(,pid . ,instance))
