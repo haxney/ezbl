3337e1a7ad23a894f51d8574bc303af5e360ff43
Dan Hackney <dan@haxney.org>
2009-09-27 22:01:44 -0400

    Bump version number for future version.
    Signed-off-by: Dan Hackney <dan@haxney.org>

--- ezbl.el
+++ ezbl.el
@@ -2,7 +2,7 @@
 ;;
 ;; Author: Daniel Hackney
 ;; Copyright (C) 2009 Daniel Hackney
-;; Version: 0.2
+;; Version: 0.3
 ;; URL: http://github.com/dhax/ezbl/tree/master
 
 ;; This file is free software; you can redistribute it and/or modify it under
