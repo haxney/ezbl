f22460e2ca7c3d33551159d55a17a5a0bbaf6b3b
Dan Hackney <dan@haxney.org>
2011-06-02 14:46:03 -0400

    Update github url.
    My username changed, so update the change here to reflect the difference.
    
    Signed-off-by: Dan Hackney <dan@haxney.org>

--- ezbl.el
+++ ezbl.el
@@ -1,7 +1,7 @@
 ;;; ezbl.el --- Emacs interface for Uzbl (uzbl.org)
 ;;
 ;; Version: 0.4
-;; URL: http://github.com/dhax/ezbl
+;; URL: http://github.com/haxney/ezbl
 
 ;; This file is free software; you can redistribute it and/or modify it under
 ;; the terms of the GNU General Public License as published by the Free Software
